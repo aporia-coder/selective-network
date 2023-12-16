50029983eb41690259ac91c0ee47a78b5d6de216
Zachary Gray <zachary_gray12@yahoo.com>
2023-12-16 17:09:25 +0000

    fix(eslint): sorted eslint config with some minor issues

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-typescript', 'next', 'plugin:prettier/recommended'],
+  ignorePatterns: ['.eslintrc.js'],
+  env: {
+    jest: true,
+  },
+  rules: {
+    // Make prettier code formatting suggestions more verbose.
+    'prettier/prettier': ['warn'],
+    // Disable <Fragment> => <> replacement. Feel free to change
+    'react/jsx-fragments': 'off',
+    // Disable prefer default export
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/object-curly-spacing': 'off',
+    'react/jsx-filename-extension': [0],
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        ecmaVersion: 12,
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+}
