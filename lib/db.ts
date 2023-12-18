b4b456065d1286efddf97f738e73a085a523f0b2
Zachary Gray <zachary_gray12@yahoo.com>
2023-12-18 07:42:55 +0000

    feat(lib): created db util for hot releading a dn prisma client

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+// util to stop new prismaclients being created on hot relead
+
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var primsa: PrismaClient | undefined
+}
+
+export const db = globalThis.primsa || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalThis.primsa = db
