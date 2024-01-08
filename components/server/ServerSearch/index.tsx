717d4b6d62d4d6cba3c23cbde4486720545907b2
Zachary Gray <zachary_gray12@yahoo.com>
2024-01-08 17:25:22 +0000

    feat(server search): created server search component
    allows searching for members and channels

---
+++ components/server/ServerSearch/index.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { Search } from 'lucide-react'
+import { ServerSearchProps, ServerSearchTypes } from './types'
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
+import { useEffect, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+
+const ServerSearch = ({ data }: ServerSearchProps) => {
+  const [open, setOpen] = useState(false)
+  const { serverId } = useParams()
+  const router = useRouter()
+
+  // needs fixing
+  useEffect(() => {
+    const keyDown = (e: KeyboardEvent) => {
+      if (e.key === 'k' && e.ctrlKey) {
+        e.preventDefault()
+        setOpen((prev) => !prev)
+      }
+    }
+
+    document.addEventListener('keydown', keyDown)
+
+    return () => {
+      document.removeEventListener('keydown', keyDown)
+    }
+  }, [])
+
+  const onClick = (id: string, type: ServerSearchTypes) => {
+    setOpen(false)
+
+    if (type === ServerSearchTypes.MEMBER) {
+      router.push(`/servers/${serverId}/conversations/${id}`)
+    } else {
+      router.push(`/servers/${serverId}/channels/${id}`)
+    }
+  }
+
+  return (
+    <>
+      <button
+        className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/50 dark:hover:bg-zinc-700/50 transition"
+        onClick={() => setOpen(true)}
+      >
+        <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-400 dark:group-hover:text-zinc-300 transition">
+          Search
+        </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span>SHIFT</span>K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search for channels and members" />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {data.map(({ label, type, data: innerData }) => {
+            if (innerData?.length === 0) return null
+
+            return (
+              <CommandGroup key={label}>
+                {innerData?.map(({ icon, name, id }) => (
+                  <>
+                    <CommandItem onSelect={() => onClick(id, type)}>
+                      {icon}
+                      <span>{name}</span>
+                    </CommandItem>
+                  </>
+                ))}
+              </CommandGroup>
+            )
+          })}
+        </CommandList>
+      </CommandDialog>
+    </>
+  )
+}
+
+export default ServerSearch
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,14 +1,40 @@
 import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
-// import { ChannelType } from '@prisma/client'
 import { redirect } from 'next/navigation'
 import ServerHeader from './server-header'
+import { ScrollArea } from '../ui/scroll-area'
+import ServerSearch from './ServerSearch'
+import { useMemo } from 'react'
+import { ChannelType, MemberRole } from '@prisma/client'
+import { ServerSearchTypes } from './ServerSearch/types'
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from 'lucide-react'
 
 interface ServerSidebarProps {
   serverId: string
 }
 
 const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
+  const channelTypeIconList = useMemo(() => {
+    return {
+      [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+      [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+      [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
+    }
+  }, [])
+
+  const roleIconList = useMemo(() => {
+    return {
+      [MemberRole.GUEST]: null,
+      // make this into icon component as it is repeated
+      [MemberRole.MODERATOR]: (
+        <ShieldCheck className="mr-2 h-4 w-4 text-indigo-500" />
+      ),
+      [MemberRole.ADMIN]: (
+        <ShieldAlert className="mr-2 h-4 w-4 text-rose-500" />
+      ),
+    }
+  }, [])
+
   const profile = await currentProfile()
   if (!profile) redirect('/')
 
@@ -33,29 +59,80 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
     },
   })
 
-  // const textChannels = server?.channels.filter(
-  //   (channel) => channel.type === ChannelType.TEXT
-  // )
-  // const audioChannels = server?.channels.filter(
-  //   (channel) => channel.type === ChannelType.AUDIO
-  // )
-  // const videoChannels = server?.channels.filter(
-  //   (channel) => channel.type === ChannelType.VIDEO
-  // )
+  const textChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  )
+  const audioChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  )
+  const videoChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  )
 
   // remove ourself from member list
-  // const members = server?.members.filter(
-  //   (member) => member.profileId !== profile.id
-  // )
+  const members = server?.members.filter(
+    (member) => member.profileId !== profile.id
+  )
+
+  const textChannelSearchData = {
+    label: 'Text Channels',
+    type: ServerSearchTypes.CHANNEL,
+    data: textChannels?.map(({ id, name, type }) => ({
+      id,
+      name,
+      icon: channelTypeIconList[type],
+    })),
+  }
+
+  const audioChannelSearchData = {
+    label: 'Voice Channels',
+    type: ServerSearchTypes.CHANNEL,
+    data: audioChannels?.map(({ id, name, type }) => ({
+      id,
+      name,
+      icon: channelTypeIconList[type],
+    })),
+  }
+
+  const videoChannelSearchData = {
+    label: 'Video Channels',
+    type: ServerSearchTypes.CHANNEL,
+    data: videoChannels?.map(({ id, name, type }) => ({
+      id,
+      name,
+      icon: channelTypeIconList[type],
+    })),
+  }
+
+  const membersSearchData = {
+    label: 'Members',
+    type: ServerSearchTypes.MEMBER,
+    data: members?.map(({ profile: { id, name }, role }) => ({
+      id,
+      name,
+      icon: roleIconList[role],
+    })),
+  }
 
   const role = server?.members?.find(
     (member) => member.profileId === profile.id
   )?.role
+
   if (!server) redirect('/')
+
+  const data = [
+    textChannelSearchData,
+    audioChannelSearchData,
+    videoChannelSearchData,
+    membersSearchData,
+  ]
 
   return (
     <div className="h-full flex flex-col w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
+      <ScrollArea className="px-3">
+        <ServerSearch data={data} />
+      </ScrollArea>
     </div>
   )
 }
