d2db004bd039dc9fe83c2f2de5bbeef44edd3020
Zachary Gray <zachary_gray12@yahoo.com>
2024-01-13 16:22:45 +0000

    feat(channel page): redirect to home if no members or channels found

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,36 @@
-const ChannelPage = () => {
-  return <div>ChannelPage</div>
+import { APP_URL } from '@/app/settings'
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const ChannelPage = async ({
+  params: { serverId, channelId },
+}: {
+  params: { serverId: string; channelId: string }
+}) => {
+  const profile = await currentProfile()
+
+  if (!profile)
+    return redirectToSignIn({
+      returnBackUrl: APP_URL,
+    })
+
+  const channel = await db.channel.findUnique({
+    where: {
+      serverId,
+      id: channelId,
+    },
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId,
+      profileId: profile.id,
+    },
+  })
+
+  if (!channel || !member) return redirect('/')
 }
 
 export default ChannelPage
